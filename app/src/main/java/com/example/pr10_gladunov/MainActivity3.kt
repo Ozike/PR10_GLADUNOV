727aef2d582c04533d5be9e11288a5c45ba51776
Vladdisllvv13 <ostashkovladislav@yandex.ru>
2022-05-07 10:04:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pr10_gladunov/MainActivity3.kt
@@ -0,0 +1,11 @@
+package com.example.pr10_gladunov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`map.xml`)
+    }
+}
